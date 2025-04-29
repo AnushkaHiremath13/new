7101de0c86df105ee56376b7ec78814b4c7172d7
AnushkaHiremath13 <anushkahiremath13@gmail.com>
2025-04-29 10:12:35 +0530

    save

---
+++ Calculate.java
@@ -0,0 +1,16 @@
+public class Calculate {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        
+        System.out.println("Sum: " + (a+b));
+       
+        System.out.println("Difference: " + (a-b));
+       
+        System.out.println("Product: " + (a*b));
+
+        System.out.println("divion: " + (a/b));
+        
+
+    }
+}
